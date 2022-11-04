08cf6f37ad0cb99b7e43e4bebb390224de2e2651
vik0t0r <al404303@uji.es>
2022-11-04 20:21:22 +0100

    added working splash screen

--- app/src/main/java/com/example/testhackathon/MainActivity.java
+++ app/src/main/java/com/example/testhackathon/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.core.splashscreen.SplashScreen;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -14,11 +16,14 @@ import com.example.testhackathon.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SplashScreen splashScreen;
+
     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        splashScreen = SplashScreen.installSplashScreen(this);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -27,8 +32,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
